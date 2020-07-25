388ff1bea36936697c1e39a4963c5cd6f633a63e
danielVg <danielveragi@gmail.com>
2020-07-25 21:59:47 +0200

    initial project setup

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
-import 'screens/feature/index.dart';
+import 'screens/home/index.dart';
 
 class Routes {
-  final routes = <String, WidgetBuilder>{
-    '/Feature': (BuildContext context) => new Feature()
-  };
 
   Routes() {
     runApp(new MaterialApp(
-      title: 'Flutter Template',
-      routes: routes,
-      home: new Feature(),
+      title: 'WhatsUpIn',
+      home: new Home(),
     ));
   }
 }
