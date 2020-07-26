1b30861db436da521b908468648c33feeb77e0a5
danielVg <danielveragi@gmail.com>
2020-07-26 11:54:58 +0200

    basic home layout

--- lib/screens/home/Home.dart
+++ lib/screens/home/Home.dart
@@ -1,22 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:whatsUpIn/models/news/News.dart';
+import '../../api/news/NewsAPI.dart';
 
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
 
-class Home extends StatelessWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  Future<News> futureNews;
+
+  @override
+  void initState() { 
+    super.initState();
+    futureNews = fetchNews(); 
+  }
 
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Flutter template"),
+        title: new Text("Latest news:"),
       ),
       body: new Container(
         child: new Center(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text("WhatsUpIn?")
-            ],
-          )
+          child: FutureBuilder(
+            future: futureNews,
+            builder: (BuildContext context, AsyncSnapshot snapshot) {
+              if (snapshot.hasData) {
+                return (snapshot.data.Title);
+              } else if(snapshot.hasError) {
+                return Text("${snapshot.error}");
+              }
+
+              return CircularProgressIndicator();
+            },
+          ),
         )
       )
     );
