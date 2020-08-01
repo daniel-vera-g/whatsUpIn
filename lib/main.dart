99c568bf8951adb4a9a37fd72896859b4f7ed17a
danielVg <danielveragi@gmail.com>
2020-08-01 12:28:54 +0200

    new main file with starter code for app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,50 @@
-import 'routes.dart';
+import 'package:flutter/material.dart';
+import 'package:whatsUpIn/repositories/repositories.dart';
+import 'package:whatsUpIn/repositories/news/news_api_client.dart';
+import 'package:http/http.dart' as http;
+import 'package:bloc/bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:whatsUpIn/bloc/bloc.dart';
+import 'screens/home/Home.dart';
+
+// Log the current state transition
+class SimpleBlocDelegate extends BlocDelegate {
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+}
 
 void main() {
-  new Routes();
+  final NewsRepository repository =
+      NewsRepository(newsApiClient: NewsApiClient(httpClient: http.Client()));
+
+  BlocSupervisor.delegate = SimpleBlocDelegate();
+  runApp(App(repository: repository));
+}
+
+class App extends StatelessWidget {
+  final NewsRepository repository;
+
+  const App({Key key, @required this.repository})
+      : assert(repository != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WhatsUpIn?',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('WhatsUpIn?')
+        ),
+        body: BlocProvider(
+          create: (context) => NewsBloc(repository: repository),
+          child: Home(),
+          ),
+      )
+    );
+  }
 }
